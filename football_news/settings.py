ac5fda620e9da2e55d862a5a9dc0d6eb699ad636
azzzfath <AzzamFathurrahman23@gmail.com>
2025-08-27 15:23:01 +0700

    Host Salah

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://muhammad-azzam42-footballnews.pbp.cs.ui.ac.id/"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-azzam42-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
