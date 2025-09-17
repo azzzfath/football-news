b416bc11401deb1f5ba0d9b57bd645500cfb2f7f
azzzfath <AzzamFathurrahman23@gmail.com>
2025-09-17 15:18:20 +0700

    tutorial 3

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-azzam42-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://pbp.cs.ui.ac.id/muhammad.azzam42/footballnews"
+    "https://muhammad-azzam42-footballnews.pbp.cs.ui.ac.id/"
 ]
 # Application definition
 
